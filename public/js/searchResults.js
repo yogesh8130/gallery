6ee30f8269e10dc57a2eb9b9630db676b3c001a5
yogesh8130 <yogesh8130@gmail.com>
2023-05-10 19:37:51 +0530

    remove duplicate function that broke fullscreen

--- public/js/searchResults.js
+++ public/js/searchResults.js
@@ -101,12 +101,6 @@ document.addEventListener('keydown', function (event) {
 });
 
 document.addEventListener('keydown', function (event) {
-	if (event.key === 'f' || event.key === 'F') {
-		showHideHeadingDiv()
-	}
-});
-
-document.addEventListener('keydown', function (event) {
 	if (event.key === '1') {
 		showAllImagesAtActualSize();
 	}
