42e36ce881d3f9f19332f0800fdc7ad8963b84dd
yogesh8130 <yogesh8130@gmail.com>
2023-05-17 17:33:48 +0530

    created new settings page for global configs

---
+++ public/js/config.js
@@ -0,0 +1,31 @@
+function refreshDB() {
+
+	const refreshDBButton = document.getElementById('refreshDBButton');
+	refreshDBButton.classList.add('processing');
+
+	fetch('/refreshDB')
+		.then(response => {
+			if (response.ok) {
+				console.log('Database refreshed successfully');
+				refreshDBButton.classList.add('success');
+				refreshDBButton.classList.remove('processing');
+				refreshDBButton.classList.remove('default');
+				setTimeout(() => {
+					refreshDBButton.classList.remove('success');
+					refreshDBButton.classList.add('default');
+				}, 3000);
+			} else {
+				refreshDBButton.classList.add('error');
+				refreshDBButton.classList.remove('default');
+				refreshDBButton.classList.remove('processing');
+				setTimeout(() => {
+					refreshDBButton.classList.remove('error');
+					refreshDBButton.classList.add('default');
+				}, 3000);
+				throw new Error('Error refreshing database');
+			}
+		})
+		.catch(error => {
+			console.error(error);
+		});
+}
